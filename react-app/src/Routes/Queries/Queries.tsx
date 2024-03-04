30216dd96fecc2f8a497f8842f1af97b6320eb92
Trevor Farias <levelonethousandr@gmail.com>
2024-03-04 00:16:24 -0500

    working database queries

--- react-app/src/Routes/Queries/Queries.tsx
+++ react-app/src/Routes/Queries/Queries.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
+const TaskInput: React.FC = () => {
+    const handleClick = async () => {
+        const description = window.prompt('Enter task description:');
+        if (description) {
+            const response = await fetch('http://localhost:3300/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ description })
+            });
+            if (response.ok) {
+                console.log('Task added successfully!');
+            } else {
+                console.error('Failed to add task:', response.statusText);
+            }
+        }
+    };
 
-const Queries: React.FC = () =>{
     return (
-      <>
-      
-      </>
-    )
-  }
+        <div>
+            <button onClick={handleClick}>Add Task</button>
+        </div>
+    );
+};
 
-  export default Queries;
+export default TaskInput;
