4bcce832717f8bec66eddc84802c28d09a3415bf
Trevor Farias <levelonethousandr@gmail.com>
2024-03-28 20:26:48 -0400

    good

--- react-app/src/Routes/Home/ProjectPage/ProjectPage.tsx
+++ react-app/src/Routes/Home/ProjectPage/ProjectPage.tsx
@@ -3,11 +3,9 @@ import Questions from './Questions.tsx'
 
 const ProjectPage: React.FC = () => { 
 
-  const acceptedArray: string[] = Questions();
-  console.log(acceptedArray);
 
   return(<>
-  
+  <Questions />
   </>)
 }
 export default ProjectPage
--- react-app/src/Routes/Home/ProjectPage/Questions.tsx
+++ react-app/src/Routes/Home/ProjectPage/Questions.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './Questions.css'
+const Questions:React.FC = () => {
+  const myArray = [['Question1', 'Question2'], ['test1', 'test2']];
+  return (
+    <>
+    <h1>Project</h1>
+    <section className="questions-container">
 
-function Questions(): string[] {
-  const myArray = ['1', '2'];
-  return myArray;
+    <div className="questions">
+        <h1>Questions</h1>
+      {myArray[0].map((question, index) => (
+        <div key={index}>{question}</div>
+      ))}
+    </div>
+    
+
+    <div className="questions-list">
+    <h1>Answers</h1>
+    {myArray[1].map((answer, index) => (
+          <div key={index}>{answer}</div>
+        ))}
+    </div>
+    </section>
+    </>
+  ) 
 }
-
 export default Questions
